8823ce23cddbe8999e07034bc3ba20bb11f88c16
diego <software.fe@grupoccima.com.com>
2023-05-09 09:24:59 -0600

    mensaje de confirmacion

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const btnregistro = document.getElementById("guardar");
-
-btnregistro.addEventListener("click", () => {
-    a = document.getElementsById("formulario_hero")
-    a.style.display = "none";
-    
-    b = document.getElementsById("gracias_hero")
-    b.style.display = "block";
-});
