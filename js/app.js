bfb014a0ac2eb1ea704f8af5c526f91c17c58535
diego <software.fe@grupoccima.com.com>
2023-05-08 18:15:07 -0600

    cambios de vista Movil

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const btnregistro = document.getElementById("guardar");
+
+btnregistro.addEventListener("click", () => {
+    a = document.getElementsById("formulario_hero")
+    a.style.display = "none";
+    
+    b = document.getElementsById("gracias_hero")
+    b.style.display = "block";
+});
